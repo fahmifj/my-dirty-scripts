380c5f484a05993577b6b3f9ee0e1976ac6012a5
Fahmi FJ <fahmifj@protonmail.com>
2022-08-27 23:01:31 +0700

    reorganize, add more submodule

---
+++ powershell/tetheron.ps1
@@ -0,0 +1,21 @@
+﻿# https://stackoverflow.com/questions/57899096/tetheringwifiband-in-windows-networking-networkoperators
+
+$connProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()
+if ($connProfile -eq $null){
+    # Write-Host "No internet connection available"
+    return 
+}
+Write-Host $connProfile.ProfileName
+
+$tetherMgr = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connProfile)
+$hotspotStatus = $tetherMgr.TetheringOperationalState
+Write-Host "Hotspot Status: " $hotspotStatus
+
+if ($hotspotStatus -eq "On") {
+    Write-Host "Hotspot already active"
+    return
+}
+
+$tetherMgr.StartTetheringAsync()
+
+#([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
